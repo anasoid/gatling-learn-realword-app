1223b32c6c762ad2eea164094bb8d6394a3762b7
anasoid <oufdou.anas@gmail.com>
2026-08-14 13:59:58 +0100

    add getLast

---
+++ gatling-app/src/gatling/java/org/anasoid/learn/gatling/app/api/UserAndAuthenticationApi.java
@@ -0,0 +1,43 @@
+package org.anasoid.learn.gatling.app.api;
+
+import com.realworld.gatling.generated.api.GeneratedUserAndAuthenticationApi;
+import com.realworld.gatling.generated.model.LoginUserEnvelope;
+import com.realworld.gatling.generated.model.UserEnvelope;
+import io.gatling.javaapi.core.ChainBuilder;
+
+/*
+ * Copyright 2023-2026 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * @author : anasoid
+ * Date :   8/14/26
+ */ public class UserAndAuthenticationApi extends GeneratedUserAndAuthenticationApi {
+
+  public static final String AUTH_TOKEN = "authToken";
+  private static UserAndAuthenticationApi instance = new UserAndAuthenticationApi();
+
+  public static UserAndAuthenticationApi instance() {
+    return instance;
+  }
+
+  @Override
+  public ChainBuilder login(LoginUserEnvelope body) {
+    return loginAndParseResponse(body)
+        .exec(
+            session -> {
+              UserEnvelope userEnvelope = getLastResponseFromlogin(session);
+              session.set(AUTH_TOKEN, userEnvelope.getUser().getToken());
+              return session;
+            });
+  }
+}
