7d0d1719215bd2808ac79f5ab9f2db129a1546ae
anasoid <oufdou.anas@gmail.com>
2026-08-19 14:22:27 +0100

    init api

---
+++ gatling-app/src/gatling/java/org/anasoid/learn/gatling/app/api/ArticlesApi.java
@@ -0,0 +1,29 @@
+package org.anasoid.learn.gatling.app.api;
+
+import com.realworld.gatling.generated.api.GeneratedArticlesApi;
+import io.gatling.javaapi.http.HttpRequestActionBuilder;
+import org.anasoid.learn.gatling.app.util.AuthenticationUtil;
+
+/*
+ * Copyright 2023-2026 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * @author : anasoid
+ * Date :   8/19/26
+ */ public class ArticlesApi extends GeneratedArticlesApi {
+
+  @Override
+  protected HttpRequestActionBuilder builderAuthentication(HttpRequestActionBuilder builder) {
+    return AuthenticationUtil.builderAuthentification(builder);
+  }
+}
