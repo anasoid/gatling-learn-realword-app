998036ac95f4ea3b9bad8fd0af5c8e701cac3883
anasoid <oufdou.anas@gmail.com>
2026-08-20 23:49:13 +0100

    init articles

--- gatling-app/src/gatling/java/org/anasoid/learn/gatling/app/api/ArticlesApi.java
+++ gatling-app/src/gatling/java/org/anasoid/learn/gatling/app/api/ArticlesApi.java
@@ -1,7 +1,13 @@
 package org.anasoid.learn.gatling.app.api;
 
 import com.realworld.gatling.generated.api.GeneratedArticlesApi;
+import com.realworld.gatling.generated.model.ArticlesEnvelope;
+import io.gatling.javaapi.core.ChainBuilder;
+import io.gatling.javaapi.core.Session;
 import io.gatling.javaapi.http.HttpRequestActionBuilder;
+import java.util.HashSet;
+import java.util.Set;
+import org.anasoid.learn.gatling.app.bean.utils.ArticlePagination;
 import org.anasoid.learn.gatling.app.util.AuthenticationUtil;
 
 /*
@@ -20,10 +26,81 @@ import org.anasoid.learn.gatling.app.util.AuthenticationUtil;
  * limitations under the License.
  * @author : anasoid
  * Date :   8/19/26
- */ public class ArticlesApi extends GeneratedArticlesApi {
+ */
+public class ArticlesApi extends GeneratedArticlesApi {
+
+  public static final String ARTICLES_NEXT_PAGE = "ARTICLES_NEXT_PAGE";
+  public static final String ARTICLES_ID = "ARTICLES_ID";
+  private static final ArticlesApi instance = new ArticlesApi();
+
+  public static ArticlesApi instance() {
+    return instance;
+  }
 
   @Override
   protected HttpRequestActionBuilder builderAuthentication(HttpRequestActionBuilder builder) {
     return AuthenticationUtil.builderAuthentification(builder);
+  }
+
+  @Override
+  public ChainBuilder getArticles(
+      String tag, String author, String favorited, Integer offset, Integer limit) {
+    return getArticlesAndParseResponse(tag, author, favorited, offset, limit)
+        .exec(
+            session -> {
+              if (session.isFailed()) {
+                return session.removeAll(AuthenticationUtil.AUTH_TOKEN, ARTICLES_NEXT_PAGE);
+              }
+              Session newSession = initArticles(session);
+              Set<String> articlesId = session.getSet(ARTICLES_ID);
+              ArticlesEnvelope articlesEnvelope = getLastResponseFromGetArticles(session);
+              if (articlesEnvelope == null || articlesEnvelope.getArticles() == null) {
+                throw new IllegalStateException(
+                    "Expected login response after a successful login request");
+              }
+              removeGetArticleSavedResponses(session);
+              articlesEnvelope.getArticles().forEach(a -> articlesId.add(a.getSlug()));
+              if (articlesEnvelope.getArticlesCount() > offset) {
+                newSession.set(
+                    ARTICLES_NEXT_PAGE,
+                    ArticlePagination.builder()
+                        .tag(tag)
+                        .author(author)
+                        .favorited(favorited)
+                        .offset(offset + limit)
+                        .limit(limit)
+                        .build());
+              } else {
+                newSession = newSession.remove(ARTICLES_NEXT_PAGE);
+              }
+              return newSession.set(ARTICLES_ID, articlesId);
+            });
+  }
+
+  public Boolean isArticlesLoaded(Session session) {
+    Set<String> articlesId = session.getSet(ARTICLES_ID);
+    return (articlesId != null && !articlesId.isEmpty());
+  }
+
+  @Override
+  public ChainBuilder deleteArticle(String slug) {
+    return super.deleteArticle(slug)
+        .exec(
+            session -> {
+              Set<String> articlesId = session.getSet(ARTICLES_ID);
+              if (articlesId != null && !articlesId.isEmpty()) {
+                articlesId.remove(slug);
+              }
+              return session;
+            });
+  }
+
+  private Session initArticles(Session session) {
+    Set<String> articlesId = session.get(ARTICLES_ID);
+    if (articlesId == null) {
+      articlesId = new HashSet<>();
+      return session.set(ARTICLES_ID, articlesId);
+    }
+    return session;
   }
 }
